2ac7889f0628551ce540878a1bf413fefbed5871
amitwats <amitwats@gmail.com>
2019-12-20 11:32:50 +1100

    React App

---
+++ precache-manifest.0a69f1581ebab23203bd256b29671336.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69b4f2c7d80f3226b8eddecde62a74a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7052c32628c1aeb4f570",
+    "url": "/static/css/main.2bfb67cd.chunk.css"
+  },
+  {
+    "revision": "f25667a9c95506de0811",
+    "url": "/static/js/2.7254e417.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.7254e417.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7052c32628c1aeb4f570",
+    "url": "/static/js/main.93464962.chunk.js"
+  },
+  {
+    "revision": "cdb471905a4c85b4f014",
+    "url": "/static/js/runtime-main.483601b1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
